939294560ab5569c5aefb626a6ca3aa451fd25e3
Kelly R Cano <krcano2001@gmail.com>
2021-11-08 14:05:19 -0800

    filled in the product associtations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+});
+// Products belongToMany Tags (through ProductTag) THIS IM NOT TOO SURE ABOUT
+Product.belongsToMany(ProductTag, {
+  foreignKey: "product_tag_id",
+});
+// Tags belongToMany Products (through ProductTag) THIS IM NOT TOO SURE ABOUT
+Tag.belongsToMany(ProductTag, {
+  foreignKey: "product_tag_id",
+});
 
 module.exports = {
   Product,
